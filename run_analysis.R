3a56989477e4b1fa79e712a33db5662f10434563
Mariano Fiorentino <Mariano.fiorentino@gmail.com>
2016-01-26 16:18:17 +0100

    Check for "UCI HAR Dataset" dir
    Check for dataset, otherwise download it

--- run_analysis.R
+++ run_analysis.R
@@ -1,61 +1,61 @@
-# Creates a clean dataset from files of UCI HAR Dataset
-#
-# @param directory character with the path of the "UCI HAR Dataset"
-# @return TRUE on success, NA on failure
-#
-# @examples
-# source("run_analysis.R")
-# result <- tidy_ucihar("UCI HAR Dataset")
+## Creates a clean dataset from files of UCI HAR Dataset
+##
+## @author Fiorentino
+## @return a list with $value = TRUE on success, NA on failure
+##
+## @examples
+## source("run_analysis.R")
 
-tidy_ucihar <- function (directory = "UCI HAR Dataset") {
-
+tidy_ucihar <- function ()
+{
+    # Check if data is not in dir, otherwise downloads and unzips 
+    checkdata <- function()
+    {
+        if (!"UCI HAR Dataset"  %in% dir()) {
+            download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
+                destfile = "UCIHAR_Dataset.zip", method = "curl")
+            unzip("UCIHAR_Dataset.zip")
+        }
+        return ("UCI HAR Dataset");
+    }
+    # Local function, reads test and train datasets and binds the rows by prefix
+    bindbyprefix <- function(prefix)
+    {
+        test <- read.table(sprintf("%s/test/%s_test.txt", directory, prefix));
+        train <- read.table(sprintf("%s/train/%s_train.txt", directory, prefix));
+        bind_rows(test, train);
+    }
     result <- tryCatch({
         # loads the required libraries
         library(dplyr)
-        library(tidyr)
+        # download the file
+        directory = checkdata();
         # reads test and train datasets and binds the rows
-        test <- read.table(sprintf("%s/test/X_test.txt", directory));
-        train <- read.table(sprintf("%s/train/X_train.txt", directory));
-        binded <- bind_rows(test, train);
-        # reads the features
+        binded <- bindbyprefix("X")
         tbl <- tbl_df(binded);
+        # reads features names
         names_df <- read.table(sprintf("%s/features.txt", directory));
-        # selects the features names by mean & std        
-        tbl %>% 
-            select(grep('-(mean|std)\\(\\)', names_df[,2], perl = TRUE)) -> tbl
-        # cleans names with some regexpr and adds column name to the dataset
+        # selects columns indexes by mean & std        
+        tbl %>% select(grep('-(mean|std)\\(\\)', names_df[,2], perl = TRUE)) -> tbl
+        # cleans names with some regexpr and adds column names to the dataset
         names <- names_df[grepl('-(mean|std)\\(\\)', names_df[,2], perl = TRUE),2]
-        names <- sub("^t(.*)", "time.\\1", names)
-        names <- sub("^f(.*)", "frequency.\\1", names)
-        names <- gsub("\\(\\)", "", names)
-        names <- gsub("-", ".", names)
-        names <- tolower(names)
+        patterns <- c("^t(.*)",   "^f(.*)",        "\\(\\)", "-",   "(.*)")
+        repl     <- c("time.\\1", "frequency.\\1", "",       ".",   "\\L\\1")
+        for (idx in seq_along(patterns)) names = gsub(patterns[idx], repl[idx], names, perl = TRUE)
         names(tbl) <- names
         # reads "activity" column from train & test files
-        test_act <- read.table(sprintf("%s/test/y_test.txt", directory));
-        train_act <- read.table(sprintf("%s/train/y_train.txt", directory));
-        binded_act <- bind_rows(test_act,  train_act)
+        binded_act <- bindbyprefix("y")
         names(binded_act) <- c("activity")
         # read "subject" column for train & test        
-        test_subj <- read.table(sprintf("%s/test/subject_test.txt", directory));
-        train_subj <- read.table(sprintf("%s/train/subject_train.txt", directory));
-        binded_subj <- bind_rows(test_subj,  train_subj)
+        binded_subj <- bindbyprefix("subject")
         names(binded_subj) <- c("subject")
         # joins the main table with the activity and subject datasets
-        tbl <- bind_cols(
-            binded_subj, 
-            binded_act, 
-            tbl
-        )
+        tbl <- bind_cols(binded_subj, binded_act, tbl)
         # reads activities labels and assigns it to the table 
         desc_act <- read.table(sprintf("%s/activity_labels.txt", directory));
         tbl$activity <- factor(tbl$activity, desc_act[,1], desc_act[,2])
-        # groups by subject & activity and gather information using key=observation, value=average
-        tbl %>% 
-            group_by(subject, activity) %>% 
-            summarise_each(funs(mean)) %>%
-            gather(key=observation, value=average, -subject, -activity) %>%
-            arrange(subject, activity) -> tbl
+        # groups by subject & activity and summarise_each
+        tbl %>% group_by(subject, activity) %>% summarise_each(funs(mean)) -> tbl 
         write.table(tbl, file="tidy_ucihar.txt", row.name=FALSE)
         return(TRUE)
     }, warning = function(err) {
@@ -63,7 +63,7 @@ tidy_ucihar <- function (directory = "UCI HAR Dataset") {
         print(paste("WARNING:  ",err))
         return(NA)
     }, error = function(err) {
-    
+        
         print(paste("ERROR:  ",err))
         return(NA)
     }); # END tryCatch
